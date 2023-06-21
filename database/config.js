a350363df88d94493223bff711b7979dbec4a2a8
jainaj609 <jainaj609@gmail.com>
2023-06-21 10:41:44 +0530

    added

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async()=>{
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.UserName}:${process.env.PassWord}@cluster0.0juvczz.mongodb.net/${process.env.DataBase}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology:true})
+        await mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.0juvczz.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology:true})
         console.log("Datebase connected")
     } catch (error) {
         console.log(error)
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        SERVER: process.env.SERVER,
+        USERNAME: process.env.USERNAME,
+        PASSWORD: process.env.PASSWORD,
+        DATABASE: process.env.DATABASE
+    },
+};
