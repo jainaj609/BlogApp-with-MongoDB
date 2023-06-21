5999f74fa1744fe3eb050a84d5210de905f21681
jainaj609 <jainaj609@gmail.com>
2023-06-22 01:25:59 +0530

    remove next.config.js file

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-        SERVER: process.env.SERVER,
-        USERNAME: process.env.USERNAME,
-        PASSWORD: process.env.PASSWORD,
-        DATABASE: process.env.DATABASE
-    },
-};
