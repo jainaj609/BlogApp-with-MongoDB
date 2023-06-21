3e304ef21a7b74e1dc1e3e05e19994c97c1bbe23
jainaj609 <jainaj609@gmail.com>
2023-06-22 00:32:32 +0530

    adding

--- database/config.js
+++ database/config.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-
 const connectDB = async()=>{
     try {
         await mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.0juvczz.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology:true})
@@ -9,5 +8,6 @@ const connectDB = async()=>{
         console.log(error)
     }
 }
+
 
 module.exports = {connectDB}
